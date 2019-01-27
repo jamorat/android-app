59994ee1f9cc41695690e3d9958d0b03c0bb4eec
Jack Amoratis <jackamoratis@Jacks-MacBook-Air.local>
2019-01-27 12:21:31 -0500

    Populate ListView from array

--- app/src/main/java/com/example/myapplication/HomeScreen.java
+++ app/src/main/java/com/example/myapplication/HomeScreen.java
@@ -24,144 +24,13 @@ public class HomeScreen extends AppCompatActivity {
     ArrayAdapter<String> adapter;
     ArrayList<String> arrayList;
     EditText editText;
-    List<ScoreItem> scoreData;
+    ArrayList<ScoreItem> scoreData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-
-        listView = (ListView) findViewById(R.id.ScoreListView);
-        String[]scores = {"a1","b2","c3"};
-        arrayList = new ArrayList<>(Arrays.asList(scores));
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, scores);
-        listView.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
-
-        scoreData = new List<ScoreItem>() {
-
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(@Nullable Object o) {
-                return false;
-            }
-
-            @NonNull
-            @Override
-            public Iterator<ScoreItem> iterator() {
-                return null;
-            }
-
-            @Nullable
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(@Nullable T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean add(ScoreItem scoreItem) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(@Nullable Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(@NonNull Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(@NonNull Collection<? extends ScoreItem> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(int index, @NonNull Collection<? extends ScoreItem> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(@NonNull Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(@NonNull Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public ScoreItem get(int index) {
-                return null;
-            }
-
-            @Override
-            public ScoreItem set(int index, ScoreItem element) {
-                return null;
-            }
-
-            @Override
-            public void add(int index, ScoreItem element) {
-
-            }
-
-            @Override
-            public ScoreItem remove(int index) {
-                return null;
-            }
-
-            @Override
-            public int indexOf(@Nullable Object o) {
-                return 0;
-            }
-
-            @Override
-            public int lastIndexOf(@Nullable Object o) {
-                return 0;
-            }
-
-            @NonNull
-            @Override
-            public ListIterator<ScoreItem> listIterator() {
-                return null;
-            }
-
-            @NonNull
-            @Override
-            public ListIterator<ScoreItem> listIterator(int index) {
-                return null;
-            }
-
-            @NonNull
-            @Override
-            public List<ScoreItem> subList(int fromIndex, int toIndex) {
-                return null;
-            }
-        };
-
-
+        scoreData = new ArrayList<ScoreItem>();
 
         //Males
         scoreData.add(new ScoreItem("Ryan",63,12345678993223L));
@@ -173,6 +42,20 @@ public class HomeScreen extends AppCompatActivity {
         scoreData.add(new ScoreItem("Jess",93,1540341751000L));
         scoreData.add(new ScoreItem("Carly",89,1540341651000L));
 
+        listView = (ListView) findViewById(R.id.ScoreListView);
+        System.out.println("this is scoredata item name: " + scoreData.get(0).name);
+
+        String[]scores = {};
+        arrayList = new ArrayList<>();
+        for(ScoreItem item : scoreData){
+            arrayList.add(String.format("%s %2d %2d",item.name,item.score, item.timestamp));
+            System.out.println("iteration : " + item.name);
+        }
+        System.out.println("state of array list : " + arrayList);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 
     public void showScoresButtonPressed(View v) {

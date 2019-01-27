4930130be29652ba0ef464907dc73149bb3d7e7b
Jack Amoratis <jackamoratis@Jacks-MacBook-Air.local>
2019-01-26 20:31:07 -0500

    Show score screen when button tapped

--- app/src/main/java/com/example/myapplication/HomeScreen.java
+++ app/src/main/java/com/example/myapplication/HomeScreen.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -13,6 +15,8 @@ public class HomeScreen extends AppCompatActivity {
     }
 
     public void showScoresButtonPressed(View v) {
-        System.out.println("button has been pressed");
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        System.out.println("Show scores");
     }
 }
